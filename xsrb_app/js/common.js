65cbb45d4576dbfe50dda4664b14f95c814c9996
chensong <632185407@qq.com>
2017-08-15 17:35:04 +0800

    cs

--- xsrb_app/js/common.js
+++
@@ -1,6 +0,0 @@
-	URL="http://114.55.183.75:801/index.php/Home/";
-     function getRequestParam(name) {
-		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
-		var r = window.location.search.substr(1).match(reg);
-		return r != null?decodeURIComponent(r[2]):null;
-		} 
